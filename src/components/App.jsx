50c4ede7242fd1fe686ceba30d549e9f2e891085
Gdanov777 <128631304+Gdanov777@users.noreply.github.com>
2023-12-03 22:02:52 +0200

    summaru

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
+
+import FriendList from './FriendList/FriendList';
+import friends from '../data/friends.json';
+
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+import { GlobalStyle } from './GlobalStyle';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <GlobalStyle />
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
