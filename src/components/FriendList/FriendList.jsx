ef19b029e6ef7a24dbcd18ffa0731754257ff221
Gdanov777 <128631304+Gdanov777@users.noreply.github.com>
2023-12-04 23:00:14 +0200

    summaru

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import FriendListItem from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => (
+  <ul className={css.friend_list}>
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.protoType = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
